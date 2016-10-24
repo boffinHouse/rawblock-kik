81d17b2b33995a121af90d583ae779cfa6e6d2d8
j-smit <jeroen-git@boffinhouse.com>
2016-10-24 12:37:52 +0200

    deleted docs html from project

--- gulp/css/sass.js
+++ gulp/css/sass.js
@@ -11,33 +11,47 @@ module.exports = function(paths, gulp, plugins) {
 
     const autoprefixer = require('autoprefixer');
     const postcssImport = require('postcss-import');
+    
+    function createCSS(files, dest) {
+        const isProduction = plugins.util.env.type == 'production';
+        
+        return gulp.src(files)
+    
+                   //Process Sass
+                   .pipe(plugins.sassGlob())
+                   .pipe(isProduction ? plugins.util.noop() : plugins.sourcemaps.init())
+                   .pipe(plugins.sass({
+                       errLogToConsole: true,
+                       style: 'compressed',
+                   }))
+                   .on('error', plugins.sass.logError)
+    
+                   //Process PostCSS
+                   .pipe(plugins.postcss([
+                       autoprefixer({
+                           browsers: ['last 2 version', 'ie >= 10', 'Android >= 4.3', 'Firefox ESR'],
+                           cascade: false,
+                           map: true,
+                           remove: true,
+                       }),
+                       postcssImport(),
+                   ]))
+                   .pipe(plugins.sourcemaps.write('.'))
+                   .pipe(gulp.dest(plugins.path.join(paths.dev, 'css')))
+            ;
+    }
 
     return function() {
-        const isProduction = plugins.util.env.type == 'production';
-
-        gulp.src([plugins.path.join(paths.assets.css, 'styles.scss')])
-
-            //Process Sass
-            .pipe(plugins.sassGlob())
-            .pipe(isProduction ?  plugins.util.noop() : plugins.sourcemaps.init())
-            .pipe(plugins.sass({
-                errLogToConsole: true,
-                style: 'compressed',
-            })).on('error', plugins.sass.logError)
-
-            //Process PostCSS
-            .pipe(plugins.postcss([
-                autoprefixer({
-                    browsers: ['last 2 version', 'ie >= 10', 'Android >= 4.3', 'Firefox ESR'],
-                    cascade: false,
-                    map: true,
-                    remove: true,
-                }),
-                postcssImport(),
-            ]))
-            .pipe(plugins.sourcemaps.write('.'))
-            .pipe(gulp.dest(plugins.path.join(paths.dev, 'css')))
-        ;
+        return plugins.eventStream.merge([
+            createCSS(
+                [plugins.path.join(paths.assets.css, 'styles.scss')],
+                plugins.path.join(paths.dev, 'css')
+            ),
+            createCSS(
+                [plugins.path.join(paths.assets.css, 'styleguide/styleguide.scss')],
+                plugins.path.join(paths.doc, 'css')
+            )
+        ])
     };
     
 };
