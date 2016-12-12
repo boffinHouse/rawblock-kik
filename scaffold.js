7694b5d2ebb53642d4b45eb56f3fea08ad0285cf
alexander.farkas <info@corrupt-system.de>
2016-12-12 16:26:22 +0100

    ...

--- scaffold.js
+++ scaffold.js
@@ -41,45 +41,16 @@ const groups = [
         onlyIfJs: true,
     },
     {
-        id: 'visualTests',
-        name: 'Visual tests',
-        isMinimal: false,
-    },
-    {
         id: 'docs',
-        name: 'Documentation (md and doc page)',
+        name: 'README.md Documentation',
     },
 ];
 
 const files = [
     {
         groupId: 'jsTests',
-        file: 'tests/funit/{name}-tests.html',
+        file: 'test/{name}-spec.js',
         onlyIfJs: true,
-    },
-    {
-        groupId: 'jsTests',
-        file: 'tests/funit/{name}-tests.js',
-        onlyIfJs: true,
-    },
-    {
-        groupId: 'visualTests',
-        file: 'tests/visual/test-{name}_page{isData}.hbs',
-        onlyIfData: true,
-    },
-    {
-        groupId: 'visualTests',
-        file: 'tests/visual/test-{name}_page{isNoData}.hbs',
-        onlyIfNoData: true,
-    },
-    {
-        groupId: 'visualTests',
-        file: 'tests/visual/testdata_{name}.json',
-        onlyIfData: true,
-    },
-    {
-        groupId: 'visualTests',
-        file: 'tests/visual/visualtests-{name}.js',
     },
     {
         groupId: 'sass',
@@ -124,10 +95,6 @@ const files = [
         groupId: 'data',
         file: '{fullUnderscoreName}.json',
         onlyIfData: true,
-    },
-    {
-        groupId: 'docs',
-        file: 'zdocs_{name}_page.hbs',
     },
     {
         groupId: 'docs',
@@ -205,38 +172,38 @@ function questionPreset(){
             default: () => { return 'rb'; },
         },
         {
-            type: "list",
-            name: "preset",
-            message: "What kind of component do you want to generate?",
+            type: 'list',
+            name: 'preset',
+            message: 'What kind of component do you want to generate?',
             choices: [
-                new inquirer.Separator('Minimum: (no data, no docs, no unit tests)'),
+                new inquirer.Separator('Minimum: (no data, no README, no unit tests)'),
                 {
-                    name: "- Minimal without JS",
-                    value: "min"
+                    name: '- Minimal without JS',
+                    value: 'min',
                 },
                 {
-                    name: "- Minimal with JS",
-                    value: "min_js"
+                    name: '- Minimal with JS',
+                    value: 'min_js',
                 },
-                new inquirer.Separator('Medium: (no docs, no unit tests)'),
+                new inquirer.Separator('Medium: (no README, no unit tests)'),
                 {
-                    name: "- Medium without JS",
-                    value: "medium"
+                    name: '- Medium without JS',
+                    value: 'medium',
                 },
                 {
-                    name: "- Medium with JS",
-                    value: "medium_js"
+                    name: '- Medium with JS',
+                    value: 'medium_js',
                 },
                 new inquirer.Separator('Full:'),
                 {
-                    name: "- Full without JS",
-                    value: "full"
+                    name: '- Full without JS',
+                    value: 'full',
                 },
                 {
-                    name: "- Full with JS",
-                    value: "full_js"
+                    name: '- Full with JS',
+                    value: 'full_js',
                 },
-            ]
+            ],
         },
     ];
 
@@ -257,8 +224,8 @@ function checkoutScaffold(choices){
                     return 'You must choose at least one file group.';
                 }
                 return true;
-            }
-        }
+            },
+        },
     ]).then( ( answers ) => {
         copyFiles(answers.fileGroups);
     });
@@ -276,7 +243,7 @@ function copyFiles(fileGroups){
             (file.onlyIfData && !isData) ||
             fileGroups.indexOf(file.groupId) == -1
         ){return;}
-        
+
         installPath = path.join(baseInstallPath, replaceNames(file.file));
 
         if(fs.existsSync(installPath)) {
@@ -285,13 +252,13 @@ function copyFiles(fileGroups){
         }
 
         content = replaceNames(fs.readFileSync(path.join(copyDir, file.file), 'utf8') || '');
-        
+
         fs.outputFile(installPath, content, (error) => {
             if(error) {
                 return console.log(error);
             }
         });
-        
+
     });
 
     console.log('Installed to: '+ baseInstallPath);
